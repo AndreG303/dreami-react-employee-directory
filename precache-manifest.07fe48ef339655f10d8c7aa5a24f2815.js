488afc866aab073b0a71a0c498de793aff28d1bf
Andrea Gamez <andreamgamez@gmail.com>
2020-10-17 12:31:08 -0700

    Updates

---
+++ precache-manifest.07fe48ef339655f10d8c7aa5a24f2815.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7cd21446576f24ed416cd39613835b6e",
+    "url": "/dreami-react-employee-directory/index.html"
+  },
+  {
+    "revision": "31bff8357875deebc62a",
+    "url": "/dreami-react-employee-directory/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "80da2e79c096287aa957",
+    "url": "/dreami-react-employee-directory/static/css/main.2b63ae09.chunk.css"
+  },
+  {
+    "revision": "31bff8357875deebc62a",
+    "url": "/dreami-react-employee-directory/static/js/2.ff264910.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/dreami-react-employee-directory/static/js/2.ff264910.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "80da2e79c096287aa957",
+    "url": "/dreami-react-employee-directory/static/js/main.b53a6cb5.chunk.js"
+  },
+  {
+    "revision": "2c8c4b4eb8f873cc99e8",
+    "url": "/dreami-react-employee-directory/static/js/runtime-main.933a4b05.js"
+  }
+]);
